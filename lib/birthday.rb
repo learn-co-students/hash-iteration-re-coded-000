74462dabe932ad94dfa2abe48e64addad80854bc
dina aziz <dina.aziz100@yahoo.com>
2017-01-03 21:42:28 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,3 +10,8 @@ def happy_birthday(birthday_kids)
   puts  "Happy Birthday #{kids_name}! You are now #{age} years old!"
 end
 end
+ happy_birthday (birthday_kids = {
+    "Timmy" => 9,
+    "Sarah" => 6,
+   "Amanda" => 27
+  })
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -16,3 +16,10 @@ end
 end
 winner
 end
+puts select_winner (passengers = {
+suite_a: "Amanda Presley",
+suite_b: "Seymour Hoffman",
+suite_c: "Alfred Tennyson",
+suite_d: "Charlie Chaplin",
+suite_e: "Crumpet the Elf"
+ })
